77db241e27ccdf29f8d252b610a3ab4a3c99dda8
adityatiwarig <aditya2461tiwari@gmail.com>
2025-06-18 22:05:25 +0530

    setup

---
+++ src/models/client/config.ts
@@ -0,0 +1,9 @@
+import { Client, Account } from "appwrite";
+
+const client = new Client()
+    .setEndpoint('https://<REGION>.cloud.appwrite.io/v1') // Your API Endpoint
+    .setProject('<PROJECT_ID>');                 // Your project ID
+
+const account = new Account(client);
+
+
